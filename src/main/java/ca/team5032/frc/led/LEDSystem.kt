db5ad5177dd1a9ec15359caca6b6e742572ce27e
Draem <23418831+Draem@users.noreply.github.com>
2022-03-03 16:05:52 -0500

    add property to change default LED colour

--- src/main/java/ca/team5032/frc/led/LEDSystem.kt
+++ src/main/java/ca/team5032/frc/led/LEDSystem.kt
@@ -2,24 +2,32 @@ package ca.team5032.frc.led
 
 import ca.team5032.frc.Perseverance
 import ca.team5032.frc.drive.DriveTrain
+import ca.team5032.frc.utils.DoubleProperty
+import ca.team5032.frc.utils.Tabbed
 import edu.wpi.first.wpilibj.motorcontrol.Spark
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 
-class LEDSystem : SubsystemBase() {
+class LEDSystem : SubsystemBase(), Tabbed {
 
     companion object {
 
         const val BLINKIN_ID = 9
 
+        val DEFAULT_COLOUR = DoubleProperty("Default Colour", -0.93)
+
     }
 
-    val blinkin: Spark = Spark(9)
+    val blinkin = Spark(9)
+
+    init {
+        buildConfig(DEFAULT_COLOUR)
+    }
 
     override fun periodic() {
         if (Perseverance.drive.state == DriveTrain.State.DRIVING) {
             blinkin.set(.91)
         } else {
-            blinkin.set(.69)
+            blinkin.set(DEFAULT_COLOUR())
         }
     }
 
